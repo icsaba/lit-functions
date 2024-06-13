500b970f358917fcd94760500c91ec3b4492da91
icsaba <csaba.ivancza@gmail.com>
2024-06-13 19:31:10 +0200

    added meta

--- src/utils/component.ts
+++ src/utils/component.ts
@@ -1,4 +1,4 @@
-import { CSSResult } from "lit";
+import { CSSResult, LitElement } from "lit";
 import { toDashedString } from "./utils";
 import { FunctionalLitComponent, Hooks } from "./hooks";
 import BaseElement from "./base-element";
@@ -8,7 +8,8 @@ export type Props = {
   useProp: Hooks['useProp'],
   onMount: Hooks['onMount'],
   onUnMount: Hooks['onUnMount'],
-} 
+  meta: LitElement,
+}
 
 export default function component(fn: Function, styles: CSSResult[] = []) {
   const componentName = toDashedString(fn.name);
@@ -22,6 +23,7 @@ export default function component(fn: Function, styles: CSSResult[] = []) {
     useProp: hooks.useProp.bind(hooks),
     onUnMount: hooks.onUnMount.bind(hooks),
     onMount: hooks.onMount.bind(hooks),
+    meta: hooks.litElement as LitElement
   }
 
   class ComponentClass extends BaseElement {
